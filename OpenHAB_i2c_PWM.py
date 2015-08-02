0536e0386f94c185329b23c66aec5fbf2d138b88
kurt <kurt.schoenhoff@my.jcu.edu.au>
2015-08-02 20:58:10 +1000

    Oops removed if ==1 for pwm

--- OpenHAB_i2c_PWM.py
+++ OpenHAB_i2c_PWM.py
@@ -54,8 +54,6 @@ elif args.pwmNum != -1:
     print "off %s" % str(4069-((args.valueNum*4069)/100))
     if args.valueNum == 1:
         pwm.setPWM(args.pwmNum, 0, args.valueNum)
-    else:
-        pwm.setPWM(args.pwmNum, *RLYOFF)
     quit()
 
 elif args.pwmFreq != -1:
