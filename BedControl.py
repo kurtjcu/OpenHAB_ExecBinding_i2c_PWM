728a830ab0845a905ed1324e96058c965bdac023
kurtjcu <kurt.schoenhoff@my.jcu.edu.au>
2015-11-09 08:05:41 +1000

    adding specidic control for beds

---
+++ BedControl.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+
+from Adafruit_PWM_Servo_Driver import PWM
+import time
+import sys
+import argparse
+
+# ===========================================================================
+# Example Code
+# ===========================================================================
+
+# Initialise the PWM device using the default address
+# pwm = PWM(0x40)
+# Note if you'd like more debug output you can instead run:
+pwm = PWM(0x40, debug=True)
+
+#define On 100%
+RLYON = 0, 4069
+#define Off 0%
+RLYOFF = 0, 0
+
+
+# Argument parsing
+parser = argparse.ArgumentParser(description='Controls PWM hat from Adafruit')
+parser.add_argument('-t', action="store", dest="time", default=1, type=int, nargs='?')
+parser.add_argument('-a', action="store", dest="action", default='test',nargs='?')
+parser.add_argument('-v', action="store", dest="valueNum", default=0, type=int, nargs='?')
+parser.add_argument('-r', action="store", dest="relayNum", default=-1, type=int, nargs='?')
+parser.add_argument('-p', action="store", dest="pwmNum", default=-1, type=int, nargs='?')
+parser.add_argument('-f', action="store", dest="pwmFreq", default=-1, type=int, nargs='?')
+
+
+args = parser.parse_args()
+
+slpTime = args.time
+
+
+
+
+
+if args.relayNum != -1:
+    if args.valueNum == 1:
+        pwm.setPWM(args.relayNum, *RLYON)
+    else:
+        pwm.setPWM(args.relayNum, *RLYOFF)
+    quit()
+
+elif args.pwmNum != -1:
+    pwm.setPWM(args.pwmNum, 0, (args.valueNum*4069)/100)
+    quit()
+
+elif args.pwmFreq != -1:
+    pwm.setPWMFreq(args.pwmFreq)
+    quit()
+
+# test function
+elif args.action == 'test':
+    # turn on and off  relays on all channels
+    print "in Test"
+
+    for x in range(0, 15):
+        pwm.setPWM(x, *RLYOFF)
+
+    for x in range(0, 15):
+        time.sleep(slpTime)
+        pwm.setPWM(x, *RLYON)
+
+    for x in range(0, 15):
+        time.sleep(slpTime)
+        pwm.setPWM(x, *RLYOFF)
+
+    time.sleep(slpTime)
+    quit()
+
+else:
+    print "where am i"
+    quit()
