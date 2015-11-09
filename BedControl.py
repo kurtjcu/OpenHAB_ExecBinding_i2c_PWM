523d2bce25c61328fcaf5bd009216f5afbba6bcf
kurtjcu <kurt.schoenhoff@my.jcu.edu.au>
2015-11-09 10:28:41 +1000

    added head/foot control

--- BedControl.py
+++ BedControl.py
@@ -15,24 +15,55 @@ import argparse
 pwm = PWM(0x40, debug=True)
 
 #define On 100%
-RLYON = 0, 4069
+RLYOff = 0, 4069
 #define Off 0%
-RLYOFF = 0, 0
+RLYON = 0, 0
+
+head_offset = 0
+foot_offset = 2
+
 
 
 # Argument parsing
 parser = argparse.ArgumentParser(description='Controls PWM hat from Adafruit')
-parser.add_argument('-t', action="store", dest="time", default=1, type=int, nargs='?')
-parser.add_argument('-a', action="store", dest="action", default='test',nargs='?')
-parser.add_argument('-v', action="store", dest="valueNum", default=0, type=int, nargs='?')
-parser.add_argument('-r', action="store", dest="relayNum", default=-1, type=int, nargs='?')
-parser.add_argument('-p', action="store", dest="pwmNum", default=-1, type=int, nargs='?')
-parser.add_argument('-f', action="store", dest="pwmFreq", default=-1, type=int, nargs='?')
+
+parser.add_argument('-e', action="store", dest="end_of_bed", default="head", type=str, nargs='?')
+parser.add_argument('-d', action="store", dest="direction", default="up", type=str, nargs='?')
+parser.add_argument('-s', action="store", dest="status", default="off", type=str, nargs='?')
+
 
 
 args = parser.parse_args()
 
-slpTime = args.time
+
+if end_of_bed == "head":
+    if direction == "up":
+        pwm.setPWM(head_offset + 1, *RLYOFF)
+        if status == "off":
+            pwm.setPWM(head_offset, *RLYOFF)
+        elif status == "on":
+            pwm.setPWM(head_offset, *RLYON)
+    elif direction == "down":
+        pwm.setPWM(head_offset + 1, *RLYON)
+        if status == "off":
+            pwm.setPWM(head_offset, *RLYOFF)
+        elif status == "on":
+            pwm.setPWM(head_offset, *RLYON)
+
+elif end_of_bed == "foot":
+    if direction == "up":
+        pwm.setPWM(foot_offset + 1, *RLYOFF)
+        if status == "off":
+            pwm.setPWM(foot_offset, *RLYOFF)
+        elif status == "on":
+            pwm.setPWM(foot_offset, *RLYON)
+    elif direction == "down":
+        pwm.setPWM(foot_offset + 1, *RLYON)
+        if status == "off":
+            pwm.setPWM(foot_offset, *RLYOFF)
+        elif status == "on":
+            pwm.setPWM(foot_offset, *RLYON)
+
 
 
 
